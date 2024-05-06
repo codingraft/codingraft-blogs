8c4e74b13a3b2935e83d2f371b0be29ffafd5b0e
Vaibhavg987 <78644114+Vaibhavg987@users.noreply.github.com>
2024-05-06 20:25:19 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+
+import cbLogo from '../assets/cb-logo.png'
+
+function Logo({width = '100'}) {
+  return (
+    <div><img src={cbLogo} alt="" className="logo"/></div>
+  )
+}
+
+export default Logo
